7c5b885048233156b91bfea436b55bdddf6da03c
VIKTORIIA <vktrkpb@icloud.com>
2024-11-27 15:30:33 -0400

    changes

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ class DrumKit{
     constructor(){
         this.pads = document.querySelectorAll('.pad'); 
         this.playBtn = document.querySelector('.play');
+        this.currentKick = './allSounds/kick-classic.wav';
+        this.currentSnare = './allSounds/snare-acoustic01.wav';
+        this.currentHihat = './allSounds/hihat-acoustic01.wav';
         this.kickAudio = document.querySelector('.kick-sound');
         this.snareAudio = document.querySelector('.snare-sound');
         this.hihatAudio = document.querySelector('.hihat-sound');
         this.index = 0;
         this.bpm = 150;
+        this.isPlaying = null;
+        this.selects = document.querySelectorAll('select');
+        this.muteBtns = document.querySelectorAll('.mute');
+        this.tempoSlider = document.querySelector('.tempo-slider');
+
     }
     activePad(){
     this.classList.toggle('active');
@@ -37,17 +45,99 @@ class DrumKit{
         this.index++;
     }
 
-    start(){
+    start(){ 
         const interval = (60/this.bpm) * 1000;
-        setInterval(() => {
-            this.repeat();
-        }, interval);
+        if(!this.isPlaying) {
+            this.isPlaying = setInterval(() => {
+                this.repeat();
+            }, interval);
+        }
+        else{
+            clearInterval(this.isPlaying);
+            this.isPlaying = null;
+        }
+        
+    }
+    updateBtn(){
+        if(!this.isPlaying){
+            this.playBtn.innerHTML = "Stop";
+            this.playBtn.classList.add('active');
+        } 
+        else{
+            this.playBtn.innerHTML = "Play";
+            this.playBtn.classList.remove('active');
+        }
+    }
+
+    changeSound(e){
+        const selectionName = e.target.name;
+        const selectionValue = e.target.value;
+
+        switch(selectionName){
+            case 'kick-select':
+                this.kickAudio.src = selectionValue;
+                break;
+            case 'snare-select':
+                    this.snareAudio.src = selectionValue;
+                break;
+            case 'hihat-select':
+                        this.hihatAudio.src = selectionValue;
+            break;
+            
+        }
+
+        
+
+
+    }
+
+    mute(e){
+        const muteIndex = e.target.getAttribute("data-track");
+        e.target.classList.toggle('active');
+        if(e.target.classList.contains('active')){
+            switch(muteIndex){
+                case '0':
+                this.kickAudio.volume = 0;
+                break;
+                case '1':
+                this.snareAudio.volume = 0;
+                break;
+                case '2':
+                this.hihatAudio.volume = 0;
+                break;
+
+            }
+ 
+        }
+        else{
+            switch(muteIndex){
+                case '0':
+                this.kickAudio.volume = 1;
+                break;
+                case '1':
+                this.snareAudio.volume = 1;
+                break;
+                case '2':
+                this.hihatAudio.volume = 1;
+                break;
+            }
+        }
+
+    }
+
+    changeTempo(e){
+        const tempoText = document.querySelector('.tempo-nr');
+         this.bpm = e.target.value;
+         tempoText.innerHTML = e.target.value;
+
     }
 };
     
 
 
 const drumkit = new DrumKit();
+
+
 
 drumkit.pads.forEach(pad => {
     pad.addEventListener('click', drumkit.activePad);
@@ -58,8 +148,30 @@ drumkit.pads.forEach(pad => {
 })
 
 drumkit.playBtn.addEventListener("click", function(){
+    drumkit.updateBtn();
+
     drumkit.start();
 });
+
+drumkit.selects.forEach(select => {
+    select.addEventListener('change', function(e){
+        drumkit.changeSound(e);
+    })
+})
+
+drumkit.muteBtns.forEach( btn => {
+    btn.addEventListener('click', function(e){
+        drumkit.mute(e);
+
+
+    })
+
+})
+
+drumkit.tempoSlider.addEventListener("input", function(e){
+    drumkit.changeTempo(e);
+
+})
 
 console.log();
 
